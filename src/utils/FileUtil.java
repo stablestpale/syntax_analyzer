9abf22ca78779f56d6d216da8f2dd6d399b9e3c0
stablestaple <sdzzy17186@163.com>
2021-12-11 16:18:10 +0800

    final fix

--- src/utils/FileUtil.java
+++ src/utils/FileUtil.java
@@ -24,45 +24,6 @@ public class FileUtil {
         try {
             this.split = split;
             this.reader = new BufferedReader(new FileReader(path));
-            File file = new File("output.txt");
-            if(!file.exists()) {
-                file.createNewFile();
-            }
-            this.writer = new BufferedWriter(new FileWriter(file));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /*
-     * @description: 读入文件到缓冲区
-     * @param: []
-     * @return: String
-     * @date: 20:56 2021/12/4
-     */
-    public String read() {
-        String current = "";
-        String total = "";
-        try {
-            while((current = reader.readLine()) != null) {
-                total += current + split;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return total;
-    }
-
-    /*
-     * @description: 写出字符串
-     * @param: [current]
-     * @return: void
-     * @date: 20:57 2021/12/4
-     */
-    public void write(String current) {
-        try {
-            writer.write(current);
-            writer.write("\r\n");
         } catch (IOException e) {
             e.printStackTrace();
         }
